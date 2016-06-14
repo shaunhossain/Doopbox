067c57de71cc4942a6b72be95320620531941630
xushvai <xushuai@xus.io>
2016-06-15 00:10:54 +0800

    some change.
    Signed-off-by: xushvai <xushuai@xus.io>

---
+++ myapp/app.js
@@ -0,0 +1,40 @@
+var webhdfs = require('webhdfs');
+
+
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', function (request, response) {
+	response.render('index', { title: 'Doopbox', message: 'Hello there!'});
+});
+
+app.get('/webhdfs/v1/home/', function (reqest, response) {
+	var hdfs = webhdfs.createClient({
+		user: '',
+		host: '192.168.1.131',
+		port: 50070
+	});
+
+	hdfs._sendRequest('GET', 'http://192.168.1.131:50070/webhdfs/v1//?op=LISTSTATUS', '', function cb(err, res, body) {
+		response.jsonp(body);
+		// console.log(body.FileStatuses.FileStatus[0].owner);
+	});
+
+
+
+	
+
+
+	//var dir = hdfs.readdir("/home");
+	//dir.on('data', function onChunk(chunk) {
+	//	console.log(chunk)
+	//	res.jsonp({user: 'hello world!'});
+	//});
+
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
