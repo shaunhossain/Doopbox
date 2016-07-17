66eb4527450d9673c68972867e4ed322f452bc5a
xushvai <xushuai@xus.io>
2016-07-17 13:46:26 +0800

    change.
    Signed-off-by: xushvai <xushuai@xus.io>

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var session = require('express-session');
+var session = require('client-sessions');
 var webhdfs = require('webhdfs');
+var breadcrumbs = require('express-breadcrumbs');
 var swig = require('swig');
 var bodyParser = require('body-parser')
 var mysql      = require('mysql');
@@ -8,7 +9,7 @@ var mysql      = require('mysql');
 
 var app = express();
 
-var svrHost = '192.168.1.100';
+var svrHost = '10.138.50.78';
 var svrPort = 50070;
 
 var hdfs = webhdfs.createClient({
@@ -24,27 +25,33 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.use(session({secret: '1234567890'}));
+app.use(breadcrumbs.init());
+//app.use(breadcrumbs.setHome());
+app.use('/admin', breadcrumbs.setHome({
+  name: 'Dashboard',
+  url: '/admin'
+}));
 
 
 
-var sess;
+app.use(session({
+  cookieName: 'session',
+  secret: '123456789abc',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+
 
 app.get('/', function (request, response) {
+	var username = request.session.username;
+	var password = '';
 
-	sess = request.session;
-
-
-	sess.username;
-
-
-
-
-	console.log('session ==== >> ' + sess)
 
 	// response.render('index', { title: 'Doopbox', message: 'Hello there!'});
 	var page = swig.renderFile('templates/index.html', {
-		pagename: 'hahahahha'
+		username: username,
+		password: password
 	});
 	response.send(page)
 });
@@ -69,63 +76,178 @@ app.get('/home$|home*//+*', function (request, response) {
 app.get('/home*', function (request, response) {
 
 
-	console.log('========>>>>  '  +  request.session.username);
+
+	var username = request.session.username;
+	if (username == null) {
+		response.redirect('/');
+	} else {
+		var path = request.params[0];
+
+
+
+		var values = path.split('/');
+		console.log('====-------------------');
+		var url = '/home/';
+		var name = '';
+		for (i in values) {
+			if (values[i] != '') {
+				url = url + values[i] + '/';
+				name = values[i];
+				console.log('name ====>   ' + name);
+				console.log('url ====>   ' + url);
+
+				request.breadcrumbs(name, url);
+			}
+
+
+		}
+		console.log('====-------------------');
+
+			
 
 
 
 
-	var path = request.params[0];
+
+		path = '/home/' + username + path;
+
+
+		var reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=LISTSTATUS';
 
 
 
+		
 
-	path = '/home/' + 't1' + path;
-	
-	var reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=LISTSTATUS';
+		hdfs._sendRequest('GET', reqURL, '', function cb(err, res, body) {
+			var page = swig.renderFile('templates/main.html', {
+				data: body,
+				breadcrumbs: request.breadcrumbs()
+			}); 
+			response.send(page)
+		});
+	}
+});
 
+app.put('/dbox/v1/*', function (request, response) {
 
-	hdfs._sendRequest('GET', reqURL, '', function cb(err, res, body) {
+	var username = request.session.username;
+	if (username == null) {
+		response.redirect('/');
+	} else {
 
-		console.log(body);
+		var path = request.params[0];
+		var op = request.query.op;
 
-		var page = swig.renderFile('templates/main.html', {
-			data: body
-		}); 
-		response.send(page)
+		console.log('operator -----> ' + op);
 
-	});
+		var redirect_path = ('/home/' + path).slice(0, -1);
+		redirect_path = redirect_path.substring(0, redirect_path.lastIndexOf('/') + 1);
+
+		console.log("xxxx======------->  " + redirect_path);
+
+		console.log(path);
+
+		path = '/home/' + username + '/' + path;
+
+		var reqURL = '';
+
+		switch (op) {
+			case 'UPLOAD':
+				reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=CREATE&overwrite=true';
+
+				console.log('---++++++++====--> ' + reqURL);
+
+				console.log(reqURL);
+				hdfs._sendRequest('PUT', reqURL, '', function cb(err, res, body) {
+					var location = '';
+					if (res.statusCode == 307) {
+						location = res.headers.location;
+						console.log('===================>>>> ' + location);
+						response.send(location);
+					}
+				});
+				break;
+		}
+	}
 });
 
 app.get('/dbox/v1/*', function (request, response) {
+	var username = request.session.username;
+	if (username == null) {
+		response.redirect('/');
+	} else {
 
-	var path = request.params[0];
-	var op = request.query.op;
-	var redirect_path = ('/home/' + path).slice(0, -1);
-	redirect_path = redirect_path.substring(0, redirect_path.lastIndexOf('/') + 1);
+		var path = request.params[0];
+		var op = request.query.op;
 
-	console.log("xxxx======------->  " + redirect_path);
+		console.log('operator -----> ' + op);
 
-	console.log(path);
+		var redirect_path = ('/home/' + path).slice(0, -1);
+		redirect_path = redirect_path.substring(0, redirect_path.lastIndexOf('/') + 1);
 
-	path = '/home/' + 't1/' + path;
+		console.log("xxxx======------->  " + redirect_path);
 
-	var reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=' + op + '&recursive=true';
+		console.log(path);
+
+		path = '/home/' + username + '/' + path;
+
+		var reqURL = '';
 
 
-	console.log(reqURL);
-
-
-	switch (op) {
-		case 'DELETE':
-			hdfs._sendRequest('DELETE', reqURL, '', function cb(err, res, body) {
-				var page = swig.renderFile('templates/main.html', {
-					data: body
+		switch (op) {
+			case 'DELETE':
+				reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=' + op + '&recursive=true';
+				console.log(reqURL);
+				hdfs._sendRequest('DELETE', reqURL, '', function cb(err, res, body) {
+					var page = swig.renderFile('templates/main.html', {
+						data: body
+					});
+					response.redirect(redirect_path);
+					//response.send(page)
 				});
-				response.redirect(redirect_path);
-				//response.send(page)
-			});
-			break;
+				break;
+			case 'DOWNLOAD':
+				reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=OPEN';
+				hdfs._sendRequest('GET', reqURL, '', function cb(err, res, body) {
+
+					if (err) {
+						console.log(err);
+					} else {
+
+						var href = '';
+						if (res.statusCode == 200) {
+							href = res.request.uri.href;
+							console.log('-----> ' + href);
+							response.redirect(href);
+						}
+					}
+				});
+				break;
+			case 'UPLOAD':
+				reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1' + path + '?op=CREATE&overwrite=true';
+				console.log(reqURL);
+				hdfs._sendRequest('PUT', reqURL, '', function cb(err, res, body) {
+					var href = '';
+
+					console.log(err);
+					//console.log(res);
+					//console.log(body);
+
+
+					if (res.statusCode == 200) {
+						href = res.request.uri.href;
+						console.log('-----> ' + href);
+						response.send(err);
+					}
+				});
+				break;
+		}
+
+
+
 	}
+
+
 });
 
 app.get('/webhdfs/v1/*', function (request, response) {
@@ -166,7 +288,21 @@ app.put('/webhdfs/v1/*', function (request, response) {
 
 });
 
+
+
+
+
+
 app.post('/signin/', function (request, response) {
+
+
+
+
+	console.log('sessionsssss =======>  ' + request.session.passwd)
+
+
+
+
 	var username = request.body.username;
 	var password = request.body.password;
 
@@ -178,7 +314,9 @@ app.post('/signin/', function (request, response) {
 	});
 	connection.connect();
 
-	var sql = 'select * from account where username=' + '\"' + username + '\"' + 'and password=password(' + password + ')';
+	var sql = 'select * from account where username=' + '\"' + username + '\"' + ' and password=password(\"' + password + '\")';
+
+
 	connection.query(sql, function(err, results) {
   		if (err) throw err;
   		console.log('The solution is: ', results);
@@ -187,18 +325,24 @@ app.post('/signin/', function (request, response) {
   			// verify ok.
   			// response.send(username + '  ' + password);
 
-  			sess.username = username;
-
+  			request.session.username = username;
+  	
   			response.redirect('/home/');
 
 
   		} else {
-  			
+  			console.log('-------> ', results);
+
+
 
   		}
 	});
 	connection.end();
 });
+
+
+
+
 
 
 app.post('/signup/', function (request, response) {
@@ -232,7 +376,9 @@ app.post('/signup/', function (request, response) {
   				var reqURL = 'http://' + svrHost + ':' + svrPort + '/webhdfs/v1/home/' + accid + '?op=MKDIRS';
   				console.log(reqURL);
   				hdfs._sendRequest('PUT', reqURL, '', function cb(err, res, body) {
-					response.redirect('/signup/');
+					// response.redirect('/signin?username=' + username + '&' + 'password=' + password);
+					request.session.username = username;
+					response.redirect('/')
 				});  				
 			});
 		}
